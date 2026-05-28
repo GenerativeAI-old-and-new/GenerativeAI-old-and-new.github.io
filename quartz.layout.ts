5c9d5125587e301723adf8d2cf95264bba8ed9da
DarkMnDragon <1598744255@qq.com>
2026-05-28 00:32:25 -0500

    Update layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -40,7 +40,6 @@ export const defaultContentPageLayout: PageLayout = {
     //   condition: (page) => page.fileData.slug !== "index",
     // }),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
@@ -74,7 +73,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
